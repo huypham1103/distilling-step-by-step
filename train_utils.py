c5e8640cc42ad2a4bcbe9754a8e474cefff1ae54
Huy Pham <huy.pham@picap.vn>
2023-08-23 16:43:39 +0700

    add save model

--- train_utils.py
+++ train_utils.py
@@ -107,3 +107,5 @@ def train_and_evaluate(args, run, tokenizer, tokenized_datasets, compute_metrics
     
 
     trainer.train()
+    output_path = '../model_path/'
+    trainer.save_model(output_path)
