50ccd6de6522f6f91e91341c3824f87beb3a2006
NIGHT FURY <huyquochoan2@gmail.com>
2024-01-14 22:14:01 +0700

    sunday with add causal comparative

--- [API] CQA/concat.py
+++ [API] CQA/concat.py
@@ -1,26 +1,26 @@
 import os, sys
 import pandas as pd
 
-type_rationale = "neutral"
+type_rationale = "comparative"
 path = f"[API] CQA/{type_rationale}"
 
 # read all files in the directory and concatenate them
 files = os.listdir(path)
 df = pd.DataFrame()
-# df = pd.read_csv(f"[API] CQA/{type_rationale}.csv", index_col=False)
+df = pd.read_csv(f"[API] CQA/{type_rationale}.csv", index_col=False)
 
-for file in files:
-    if 'error' in file:
-        continue
-    df = pd.concat([df, pd.read_csv(f"{path}/" + file, index_col=False)])
-
-# error = pd.DataFrame()
 # for file in files:
-#     if 'error___' in file:
-#         error = pd.concat([error, pd.read_csv(f"{path}/" + file, index_col=False)])
+#     if 'error' in file:
+#         continue
+#     df = pd.concat([df, pd.read_csv(f"{path}/" + file, index_col=False)])
+
+error = pd.DataFrame()
+for file in files:
+    if 'error___' in file:
+        error = pd.concat([error, pd.read_csv(f"{path}/" + file, index_col=False)])
 
 # assign rationale from error file to df
-# df = pd.concat([df, error[['premise', 'hypothesis', 'rationale']]])
+df = pd.concat([df, error[['premise', 'hypothesis', 'rationale']]])
 
 # df = pd.concat([df, pd.read_csv("/home/huy/Desktop/HCMUS/distilling-step-by-step/[API] CQA/neutral_0.csv", index_col=False)])
 
